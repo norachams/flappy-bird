33b603f1819976aea9f7ed5fec34a75fc8b3da32
norachams <78521390+norachams@users.noreply.github.com>
2021-12-31 14:48:56 -0500

    Update Coin.java

--- Coin.java
+++ Coin.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.List;
-/**
- * Write a description of class coin here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Coin extends Actor
 {
     GifImage myGif = new GifImage ("coin-clipart-drawn-17.gif");
