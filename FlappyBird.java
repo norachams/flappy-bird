58592286af48a03361f5434b1c679b28a0b7731b
norachams <78521390+norachams@users.noreply.github.com>
2021-12-31 14:51:25 -0500

    Update FlappyBird.java

--- FlappyBird.java
+++ FlappyBird.java
@@ -1,10 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-/**
- * Write a description of class FlappyBird here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class FlappyBird extends Actor
 {
     double dy = 0;
