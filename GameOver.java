3a1e1a90cee3d2b9283f841438f6025c922a7af1
norachams <78521390+norachams@users.noreply.github.com>
2021-12-31 14:57:32 -0500

    Update GameOver.java

--- GameOver.java
+++ GameOver.java
@@ -8,14 +8,6 @@ public class GameOver extends Actor
      image.scale(400, 200);
      setImage(image);
     }
-    /**
-     * Act - do whatever the GameOver wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-       
-       
-        // Add your action code here.
-    }    
+    
+      
 }
