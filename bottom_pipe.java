3259fb3d8a5e098a35f1b13b6fcfc88a3c95e732
norachams <78521390+norachams@users.noreply.github.com>
2021-12-31 15:01:56 -0500

    Update bottom_pipe.java

--- bottom_pipe.java
+++ bottom_pipe.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class bottom_pipe here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class bottom_pipe extends Actor
 {
     int PIPE_SPEED = -4;
