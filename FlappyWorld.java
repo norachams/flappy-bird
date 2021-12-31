5d1f6721ce0949154a27a5dba59a1fe32a00a8ea
norachams <78521390+norachams@users.noreply.github.com>
2021-12-31 14:54:58 -0500

    Update FlappyWorld.java

--- FlappyWorld.java
+++ FlappyWorld.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.ArrayList;
-/**
- * Write a description of class Flappy_Bird_Background here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class FlappyWorld extends World
 {
     
