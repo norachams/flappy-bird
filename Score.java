23d60b8ba89f197e2d30c05c4eb866cceb2def19
norachams <78521390+norachams@users.noreply.github.com>
2021-12-31 15:01:37 -0500

    Update Score.java

--- Score.java
+++ Score.java
@@ -1,12 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 
-/**
- * Write a description of class Score here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class Score extends Actor
 {
    int score = 0;
